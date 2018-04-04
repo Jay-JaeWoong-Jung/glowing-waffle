6de12d2dbd488b995b69d5c8358776b5c989890e
kartikmat <kmathur@usc.edu>
2018-04-03 18:48:13 -0700

    Pushing Eclipse Version of BackEnd
    This version is configured for eclipse

---
+++ RoommateTest/src/main/java/Server.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class Server {
+    private Vector<ServerThread> serverThreads;
+
+    public static void main(String[] args){
+        Server server = new Server(6789);
+    }
+
+    public Server(int port) {
+        try {
+            System.out.println("Binding to port " + port);
+            ServerSocket ss = new ServerSocket(port);
+            System.out.println("Bound to port " + port);
+            serverThreads = new Vector<ServerThread>();
+            while (true) {
+                Socket s = ss.accept(); // blocking
+                System.out.println("Connection from: " + s.getInetAddress());
+                ServerThread st = new ServerThread(s, this);
+                serverThreads.add(st);
+            }
+        } catch (IOException ioe) {
+            System.out.println("ioe in ChatRoom constructor: " + ioe.getMessage());
+        }
+    }
+
+    public void processCommand(Command command, ServerThread thread){
+        switch (command.getCommandType()){
+            case GROUP_EVENT:
+                groupEvent(command, thread);
+        }
+
+    }
+
+
+    public void groupEvent(Command command, ServerThread thread){
+        // TODO
+        //Event to be broadcasted ;
+        broadcast(command,thread);
+
+    }
+
+    // broadcast function
+    public void broadcast(Command command, ServerThread thread) {
+        //if (message != null) {
+        if (command != null) {
+            //System.out.println(message);
+
+            for(ServerThread threads : serverThreads) {
+                if (thread != threads) {
+                    //threads.sendMessage(message);
+                    threads.sendObject(command);
+                }
+            }
+        }
+    }
+
+
+
+}
