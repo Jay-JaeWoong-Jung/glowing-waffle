26e65879aa8dddcd115aac81e07ebfcb185d3e6b
Jacob Schaider <jschaide@usc.edu>
2018-04-07 11:05:16 -0700

    Connected controllers to backend database login and signup servlets

--- src/constants/csci201Final.sql
+++ src/constants/csci201Final.sql
@@ -11,7 +11,7 @@ CREATE TABLE user (
   cell_number int(11) not null,
   emerg_number int(11) not null,
   email varchar(50) not null,
-  address varchar(50),
+  house_address varchar(50),
   house_handle varchar(11),
   permanant_address varchar(50),
   venmo_handle varchar(11),
@@ -22,4 +22,4 @@ CREATE TABLE user (
 );
 
 
-INSERT INTO user (username, password, first_name, last_name, cell_number, emerg_number, email, address, house_handle) VALUES ('aaa','aaa', 'Jay', 'Jung', '1234567890','0987654321','jaewoonj@usc.edu', '1241 W 37th PL, LA, CA, 90007, USA', 'main');
+INSERT INTO user (username, password, first_name, last_name, cell_number, emerg_number, email, house_address, house_handle) VALUES ('aaa','aaa', 'Jay', 'Jung', '1234567890','0987654321','jaewoonj@usc.edu', '1241 W 37th PL, LA, CA, 90007, USA', 'main');
