8911198d417784feb46ac065386dc7dc75a66d25
Jacob Schaider <jschaide@usc.edu>
2018-04-07 11:12:07 -0700

    untracking roommateTest

--- RoommateTest/src/main/java/Controller.java
+++
@@ -1,8 +0,0 @@
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Controller {
-    public ModelAndView HandleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws Exception;
-
-}
